90a126cac24f4c02e2bffce147b116d1da053f2e
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-08-11 18:51:41 +0530

    fix: fixed blog page heading size

--- src/theme/BlogListPage/Content/HeadingSection.tsx
+++ src/theme/BlogListPage/Content/HeadingSection.tsx
@@ -8,7 +8,7 @@ function HeadingSection(): JSX.Element {
         alt="background image"
         src={require(`@site/static/assets/bg-image2.png`).default}
       />
-      <div className="absolute top-1/3 w-full px-3 left-1/2 transform -translate-x-1/2 uppercase text-white font-bold text-4xl sm:text-6xl md:text-8xl lg:text-9xl text-center">
+      <div className="absolute top-1/3 w-full px-1 sm:px-3 left-1/2 md:left-1/2 transform -translate-x-1/2 uppercase text-white font-bold text-4xl sm:text-5xl md:text-8xl lg:text-9xl text-center">
         <div>Events and</div>
         <div>News</div>
       </div>
