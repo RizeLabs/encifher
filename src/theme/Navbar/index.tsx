bc759107f96c5f52e8557022f62b7ac3c16c4cb6
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-08-11 18:46:33 +0530

    fix: fixed navbar btn width

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -20,7 +20,7 @@ export default function Navbar({ onMenuClick }: NavbarProps): JSX.Element {
         <div className="flex gap-8 items-center">
           <a
             href="#getintouch"
-            className="flex gap-2 hover:no-underline transition-colors duration-700 justify-center font-menseal items-center hover:cursor-pointer hover:opacity-95 self-end bg-custom-gradient py-2 px-4 md:px-6 text-base md:text-lg font-semibold rounded-[60px] border-none text-white"
+            className="flex gap-2 hover:no-underline transition-colors duration-700 justify-center font-menseal items-center hover:cursor-pointer hover:opacity-95 self-end bg-custom-gradient py-2 px-2 md:px-6 text-base md:text-lg font-semibold rounded-[60px] border-none text-white"
           >
             <span>Get in touch</span>
             <img
