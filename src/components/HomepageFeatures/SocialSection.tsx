a7fb0db3bd6f03fef01f5e4434a25e33186ca1bb
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-08-24 13:17:50 +0530

    refactor: rollback social section

--- src/components/HomepageFeatures/SocialSection.tsx
+++ src/components/HomepageFeatures/SocialSection.tsx
@@ -4,7 +4,7 @@ import SocialCard from "../common/SocialCard";
 export default function SocialSection(): JSX.Element {
   return (
     <section className="bg-secondary-dark">
-    <div className="max-w-[1440px] ml-auto mr-auto lg:px-20 md:px-10 px-5 pb-16 md:pb-60 flex flex-col items-start gap-24 z-10" data-aos="fade-up">
+    <div className="max-w-[1440px] ml-auto mr-auto lg:px-20 md:px-10 px-5 pb-16 md:pb-32 flex flex-col items-start gap-24 z-10" data-aos="fade-up">
       <div className="max-w-[400px] md:w-full md:max-w-none mr-auto ml-auto flex flex-wrap gap-6 justify-center">
         <SocialCard
             socialIconUrl={require(`@site/static/assets/social/twitterIcon.webp`).default}
--- src/components/common/SocialCard.tsx
+++ src/components/common/SocialCard.tsx
@@ -14,18 +14,16 @@ const SocialCard: React.FC<SocialCardProps> = ({
   hoverColor,
 }) => {
   return (
-    <a
-      className="relative w-[290px] h-[208px] bg-transparent border border-solid border-[#5F5F68] flex flex-col justify-between"
+    <div
+      className="relative w-[290px] h-[360px] bg-transparent border border-solid border-[#5F5F68] flex flex-col justify-between"
       style={{ transition: "background-color 0.3s" }}
-      href={linkUrl}
-      target="_blank"
       onMouseEnter={(e) => e.currentTarget.style.backgroundColor = hoverColor}
       onMouseLeave={(e) => e.currentTarget.style.backgroundColor = "transparent"}
     >
       <div className="absolute top-10 left-10">
         <img src={socialIconUrl} alt="Social Icon" className="w-12 h-12" />
       </div>
-      {/* <a
+      <a
         href={linkUrl}
         className="absolute top-10 right-10 bg-white rounded-full shadow-md w-16 h-16"
         target="_blank"
@@ -35,12 +33,12 @@ const SocialCard: React.FC<SocialCardProps> = ({
           alt="Arrow Button"
           className="w-full h-full object-contain"
         />
-      </a> */}
-      <div className="absolute bottom-2 left-10  text-white text-left text-3xl font-bold pb-4">
+      </a>
+      <div className="absolute bottom-2 left-1/2 -translate-x-1/2 text-white text-center text-4xl font-bold p-4">
         {text}
       </div>
       <div className="absolute bottom-[-1px] right-[-1px] w-[30px] h-[30px] bg-secondary-dark border-solid border border-r-0 border-b-0 border-[#5F5F68]"></div>
-    </a>
+    </div>
   );
 };
 
