bca54b749a4d824522d162022b4c193c6f7a3881
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-10-13 07:03:11 +0530

    feat: updated card design

--- src/components/common/BlogCard.tsx
+++ src/components/common/BlogCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 interface CardProps {
   imageUrl?: string;
@@ -9,8 +9,15 @@ interface CardProps {
   topic: string;
 }
 
-const Card: React.FC<CardProps> = ({ imageUrl, heading, description, url, topic }) => {
-  const DEFAULT_IMAGE_URL = require(`@site/static/assets/random-image.webp`).default;
+const Card: React.FC<CardProps> = ({
+  imageUrl,
+  heading,
+  description,
+  url,
+  topic,
+}) => {
+  const DEFAULT_IMAGE_URL =
+    require(`@site/static/assets/random-image.webp`).default;
 
   return (
     <div className="max-w-[320px] mid:max-w-[400px] border hover:shadow-primary-purple border-solid border-[#5F5F68] overflow-hidden shadow-md flex flex-col bg-secondary-dark rounded-2xl">
@@ -22,9 +29,9 @@ const Card: React.FC<CardProps> = ({ imageUrl, heading, description, url, topic 
         />
       </div>
       <div className="p-8 bg-primary-dark flex flex-col">
-        <button className="bg-primary-brand text-white px-5 py-2 mb-4 capitalize flex self-start border-none font-menseal text-base font-semibold rounded-[60px] hover:pointer-events-none">
+        <h4 className="shadow-md flex w-auto font-menseal text-primary-brand text-base font-semibold hover:no-underline capitalize">
           {topic}
-        </button>
+        </h4>
         <h2 className="text-xl font-semibold text-white font-menseal uppercase mb-3">
           {heading}
         </h2>
@@ -33,7 +40,8 @@ const Card: React.FC<CardProps> = ({ imageUrl, heading, description, url, topic 
         </p>
         <Link
           to={url}
-          className="rounded-full shadow-md w-full font-menseal text-primary-brand text-base font-semibold hover:no-underline flex"
+          className="shadow-md flex w-auto text-base font-semibold hover:no-underline hover:opacity-80 hover:text-white
+          bg-primary-brand text-white px-5 py-2 mb-4 capitalize  self-start border-none font-menseal rounded-[60px]"
         >
           Read More
         </Link>
