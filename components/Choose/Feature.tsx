053b81212ac1bab4820c10b742e18b3500102d02
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-12-31 02:40:32 +0530

    fix: rm reused code

--- components/Choose/Feature.tsx
+++ components/Choose/Feature.tsx
@@ -9,29 +9,15 @@ interface FeatureProps {
 
 export const Feature = ({ id, title, description, icon }: FeatureProps) => {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-between gap-1 md:gap-[100px] w-full md:w-[70%] py-[2%] px-2 md:px-0">
-            {
-                (id % 2 === 0) && <div className="relative">
-                    <Image src='/concentric.svg' width={10} height={10} alt='' className='w-[200px] h-[200px] md:w-[300px] md:h-[300px]' />
-                    <Image src={icon} width={10} height={10} alt='' className='absolute m-auto inset-0 w-[160px] h-[160px]' />
-                </div>
-            }
-            {
-                (id % 2) && <div className="md:hidden relative mb-6 md:mb-0">
-                    <Image src='/concentric.svg' width={10} height={10} alt='' className='w-[200px] h-[200px] md:w-[300px] md:h-[300px]' />
-                    <Image src={icon} width={10} height={10} alt='' className='absolute m-auto inset-0 w-[160px] h-[160px]' />
-                </div>
-            }
-            <div className="w-full md:w-[50%] flex flex-col items-center md:items-start gap-4">
-                <h1 className="gradient-text text-2xl md:text-[32px] font-[400] text-center md:text-left">{title}</h1>
-                <p className="text-white/60 text-[16px] md:text-[20px] font-[300] leading-[32px] text-center md:text-left">{description}</p>
+        <div className="flex flex-col md:flex-row items-center justify-between gap-6 md:gap-[100px] w-full md:w-[70%] py-[2%] px-2 md:px-0">
+            <div className={`relative ${id % 2 === 0 ? 'order-first' : 'order-first md:order-last'}`}>
+                <Image src='/concentric.svg' width={10} height={10} alt='' className='w-[200px] h-[200px] md:w-[300px] md:h-[300px]' />
+                <Image src={icon} width={10} height={10} alt='' className='absolute m-auto inset-0 w-[160px] h-[160px]' />
             </div>
-            {
-                (id % 2) && <div className="hidden md:block relative">
-                    <Image src='/concentric.svg' width={10} height={10} alt='' className='w-[200px] h-[200px] md:w-[300px] md:h-[300px]' />
-                    <Image src={icon} width={10} height={10} alt='' className='absolute m-auto inset-0 w-[160px] h-[160px]' />
-                </div>
-            }
+            <div className="w-full md:w-[50%] flex flex-col items-center md:items-start gap-4">
+                <h1 className="gradient-text text-[20px] md:text-[32px] font-[400] text-center md:text-left">{title}</h1>
+                <p className="text-white/60 text-[16px] md:text-[20px] font-[300] leading-[25px] md:leading-[32px] text-center md:text-left">{description}</p>
+            </div>
         </div>
     )
 }
--- components/Suite/Suite.tsx
+++ components/Suite/Suite.tsx
@@ -17,7 +17,7 @@ export default function Suite() {
     return (
         <div className="flex flex-col gap-[2rem] px-4 md:px-[10%] mt-[8rem]">
             <h1 className="text-3xl md:text-[48px] gradient-text font-[400] mb-[2rem] text-center md:text-left">Encifher Suite</h1>
-            <div className="w-full pt-[3rem] flex flex-col md:flex-row items-start items-center items-stretch justify-around gap-8">
+            <div className="w-full pt-3 md:pt-[3rem] flex flex-col md:flex-row items-start items-center items-stretch justify-around gap-8">
                 {cards.map((card, index) => (
                     <Card key={index} icon={card.icon} title={card.title} description={card.description} />
                 ))}
