ecbfe8e5858f8c932ff61bd2921b9abedbe23102
Archisman Das <dasarchisman25@gmail.com>
2025-01-11 00:36:27 +0530

    feat: add table of content

--- components/Blogs/BlogPage.tsx
+++ components/Blogs/BlogPage.tsx
@@ -1,25 +1,63 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Navbar from "../Navbar/Navbar";
 import { blogs } from "./blogdetails";
 import Share from "../Share/Share";
+import { useEffect, useRef } from "react";
 
 export default function BlogPage({ blogIndex }) {
     const clock = "/clock.svg";
     const calendar = "/calendar.svg";
     const back = "/back.svg";
 
+    // Refs for section headings
+    const sectionRefs = useRef([]);
+
+    // Scroll to section when a ToC item is clicked
+    const scrollToSection = (index) => {
+        sectionRefs.current[index]?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    // Shorten the title to 10-15 characters
+    const shortenTitle = (title) => {
+        return title.length > 20 ? title.substring(0, 20) + "..." : title;
+    };
+
     return (
         <>
             <Navbar />
 
-            <div className="flex flex-col w-full h-full justify-center items-center">
-                <div className="flex flex-col w-[709px]" >
-                    <span className="flex flex-row text-white text-sm mb-4 cursor-pointer"
-                        onClick={() => window.location.href = `/blogs`} >
+            <div className="flex flex-row w-full h-full mt-8">
+
+                <div className="w-[33%] flex justify-center" >
+                    <div className="w-[200px] sticky top-20 ml-4 hidden md:block">
+                        <span className="text-white text-lg font-bold mb-4">Table of Contents</span>
+                        <div className="w-[200px] h-[1px] bg-white opacity-15 my-4"></div>
+                        <ul className="list-none p-0">
+                            {blogs[blogIndex].sections.map((section, index) => (
+                                <li key={index} className="mb-2">
+                                    <button
+                                        onClick={() => scrollToSection(index)}
+                                        className="text-white text-sm opacity-70 hover:opacity-100 hover:text-[#663FFF] transition-all"
+                                    >
+                                        {shortenTitle(section.header)}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                {/* Blog Content (Preserve Current Positioning and Size) */}
+                <div className="flex flex-col w-[709px]">
+                    <span
+                        className="flex flex-row text-white text-sm mb-4 cursor-pointer"
+                        onClick={() => (window.location.href = `/blogs`)}
+                    >
                         <Image src={back} width={9} height={9} alt="Back Icon" className="mr-2" />
-                        Go Back</span>
-                    <span className="text-white text-3xl text-[#663FFF]" >{blogs[blogIndex].title}</span>
+                        Go Back
+                    </span>
+                    <span className="text-white text-3xl text-[#663FFF]">{blogs[blogIndex].title}</span>
                     <span className="flex flex-row my-4 opacity-50">
                         <span className="flex flex-row text-white text-[12px] mr-6">
                             <Image src={clock} width={18} height={18} alt="Clock Icon" className="mr-2" />
@@ -30,24 +68,27 @@ export default function BlogPage({ blogIndex }) {
                             {blogs[blogIndex].date}
                         </span>
                     </span>
-                    <Image src={blogs[blogIndex].image} width={709} height={400} alt="Blog Image" className="my-12" />
-                    {
-                        blogs[blogIndex].sections.map((section, index) => (
-                            <div key={index} className="flex flex-col mb-8" >
-                                <span className="text-white text-2xl mb-4" >{section.header}</span>
-                                <div className="text-white text-base normal-case opacity-50" >
-                                    {section.content}
-                                </div>
-                            </div>
-                        ))
-                    }
+                    <Image
+                        src={blogs[blogIndex].image}
+                        width={709}
+                        height={400}
+                        alt="Blog Image"
+                        className="my-12"
+                    />
+                    {blogs[blogIndex].sections.map((section, index) => (
+                        <div
+                            key={index}
+                            className="flex flex-col mb-8"
+                            ref={(el) => (sectionRefs.current[index] = el)} // Assign ref to each section
+                        >
+                            <span className="text-white text-2xl mb-4">{section.header}</span>
+                            <div className="text-white text-base normal-case opacity-50">{section.content}</div>
+                        </div>
+                    ))}
 
-                    <div className="w-full h-[1px] bg-white opacity-50">
-                    </div>
-                    <span className="text-white text-base normal-case md:mt-16" >
-                        Share on
-                    </span>
-                    <div className="flex flex-row justify-start my-4" >
+                    <div className="w-full h-[1px] bg-white opacity-50"></div>
+                    <span className="text-white text-base normal-case md:mt-16">Share on</span>
+                    <div className="flex flex-row justify-start my-4">
                         <Share image="/telegram.svg" />
                         <Share image="/twitter.svg" />
                         <Share image="/discord.svg" />
